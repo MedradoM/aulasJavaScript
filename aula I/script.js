8ca7953e3826f957c60ef736a3c6772c2034db93
MedradoM <mat.developer@outlook.com>
2023-08-23 20:06:12 -0300

    main

---
+++ aula I/script.js
@@ -0,0 +1,29 @@
+
+    const header = document.getElementsByTagName('header');    
+
+    var nome = 'Matheus M.'; 
+    var sobrenome = 'Massena'
+    var ano = 2023;
+    var idade = 20;
+    var altura = 1.80;
+    var jaComeu = true;
+    nome = prompt('Fala teu nome ae patrão');
+    sobrenome = prompt('Meu consagrado, manda o teu sobrenome ae namoral');
+    idade = prompt('Quantos aninhos vc vai ter até o final do ano?')
+
+    var bodia = nome + " " + sobrenome;
+    var bonoite = ano - idade;
+
+
+    alert(bodia);
+    alert("Tu nasceu em: " + bonoite);
+    console.log(nome);
+    console.log(sobrenome);    
+    console.log(idade);
+    console.log(idadeFinal);
+    console.log(altura);
+    console.log(jaComeu);
+    
+
+
+
